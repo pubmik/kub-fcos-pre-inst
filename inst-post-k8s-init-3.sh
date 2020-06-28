72b29823f0b109783f5a8c64d82582e5e80ca047
pubmik <pubmik@yahoo.com>
2020-06-28 15:34:13 +0200

    type corr

--- inst-post-k8s-init-3.sh
+++ inst-post-k8s-init-3.sh
@@ -10,6 +10,6 @@ fi
 
 echo "***** join k8s cluster"
 kubeadm join ${1}:6443 --token $2 \
-    --discovery-token-ca-cert-hash $3
+    --discovery-token-ca-cert-hash $3 \
     --cri-socket /var/run/crio/crio.sock
 
